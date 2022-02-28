41a852d24f270bc3a3df2f2aa35e6f124c20a3ff
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-02-28 19:15:24 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,28 @@
+#include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
 
 namespace Statistics {
-    void ComputeStatistics(const std::vector<double>& );
+    
+    class Stats
+    {
+      public:
+        float m_avg;
+        float m_max;
+        float m_min;
+        Stats()// Default Constructor
+        {
+            
+        }
+        Stats(float avg, float max, float min)
+        {
+          m_avg = avg;
+          m_max = max;
+          m_min = min; 
+        }
+    }
+    
+    Stats ComputeStatistics(const std::vector<float>& );
 }
