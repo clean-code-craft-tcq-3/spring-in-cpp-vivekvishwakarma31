54e749bd7994125b41fadb38fb59e2a7bbc95d19
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-02-28 19:40:03 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,17 @@
 #include "stats.h"
 
-void Statistics::ComputeStatistics(const std::vector<double>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>&  data) {
     //Implement statistics here
+    
+    Stats mystats;
+    float sum;
+    int count;
+    
+    mystats.m_max = *max_element(data.begin(),data.end());
+    mystats.m_min = *min_element(data.begin(),data.end());
+    sum=accumulate(data.begin(), data.end() , 0);
+    count = data.size();
+    mystats.m_avg=(sum/count);
+    return st;
+    
 }
