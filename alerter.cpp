95b118ab0bdd1539c16fa0807fd557583d338058
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-03-03 19:20:26 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,1 +1,21 @@
+#include <alerter.h>
+#include <stats.h>
 
+
+    
+int StatsAlerter::checkAndAlert(std::vector<float> data)
+{
+        auto computedStats = Statistics::ComputeStatistics(data);
+
+        if(computedStats.m_max > m_maxThreshold)
+        {
+            alerter[0]->SetAlerter(10);
+            alerter[1]->SetAlerter(10);
+           return 1;   
+        }
+        else
+        {
+           return 0;
+        }
+        
+}
