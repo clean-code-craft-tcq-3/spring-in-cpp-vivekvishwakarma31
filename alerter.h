99956ec71cf34ab0dec422c8291156383b71b233
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-03-03 19:17:35 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,1 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class IAlerter
+{
+    public:
+    IAlerter()
+    {
+    
+    }
+    virtual void SetAlerter(int i)
+    {
+        
+    }
+};
+
+class EmailAlert : public IAlerter
+{
+    public:
+    EmailAlert()
+    {
+        DemailSent = 0;
+    }
+    void displayEmail()
+    {
+        std::cout << "DemailSent : " << DemailSent << std::endl;
+    }
+    
+    void SetAlerter(int i)
+    {
+        DemailSent = i;
+    }
+    int DemailSent;
+};
+
+class LEDAlert : public IAlerter
+{
+    public:
+    LEDAlert()
+    {
+        DledGlows = 0;
+    }
+    void displayLed()
+    {
+        std::cout << "DledGlows : " << DledGlows <<std::endl;
+    }
+    
+    void SetAlerter(int i)
+    {
+        DledGlows = i;
+    }
+    
+    int DledGlows;
+};
+
+class StatsAlerter 
+{
+    
+    public:
+    StatsAlerter()
+    {
+        
+    }
+    
+    StatsAlerter(float f1, std::vector<IAlerter*> palerters)
+    {
+        m_maxThreshold = f1;
+        alerter = palerters;
+    }
+    
+    void checkAndAlert(std::vector<float> data);
+    
+    float m_maxThreshold;
+    std::vector<IAlerter*> alerter;
+};
 
