ecb65aee8badd64fd7148ebd32547501e2763a33
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-03-03 19:25:51 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,6 +2,7 @@
 
 #include "catch.hpp"
 #include "stats.h"
+#include "alerter.h"
 
 #include <cmath>
 
@@ -37,6 +38,6 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     StatsAlerter statsAlerter(maxThreshold, alerters);
     statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
 
-    REQUIRE(emailAlert.emailSent);
-    REQUIRE(ledAlert.ledGlows);
+    REQUIRE(emailAlert.emailSent == 1);
+    REQUIRE(ledAlert.ledGlows == 1);
 }
